5d82bcce1efab8146db198e26807781100ae060b
Jan Hrcek <jhrcek@redhat.com>
2018-01-15 07:39:15 +0100

    Fix bash script warnings

--- elm/build.sh
+++ elm/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ELM_JS=dist/js/elm.js
-elm make --warn Main.elm --output=$ELM_JS
-GENERATED_ON=`date +%Y-%m-%d`
-sed -i "s/GENERATED_ON_PLACEHOLDER/$GENERATED_ON/" $ELM_JS
+elm make --warn Main.elm --output=${ELM_JS}
+GENERATED_ON=$(date +%Y-%m-%d)
+sed -i "s/GENERATED_ON_PLACEHOLDER/${GENERATED_ON}/" ${ELM_JS}
--- elm/deploy.sh
+++ elm/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 DEPLOYMENT_DIR=../../jhrcek.github.io/random-failures
-rm --recursive --force $DEPLOYMENT_DIR
-cp --recursive dist $DEPLOYMENT_DIR
+rm --recursive --force ${DEPLOYMENT_DIR}
+cp --recursive dist ${DEPLOYMENT_DIR}
 
 echo "Minifying js file"
-rm $DEPLOYMENT_DIR/js/elm.js
-uglifyjs dist/js/elm.js --compress --mangle --output $DEPLOYMENT_DIR/js/elm.js
+rm ${DEPLOYMENT_DIR}/js/elm.js
+uglifyjs dist/js/elm.js --compress --mangle --output ${DEPLOYMENT_DIR}/js/elm.js
