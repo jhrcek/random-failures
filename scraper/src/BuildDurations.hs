1fa39f64f9e6daea89ea04fe12305e1e4a55cf64
Jan Hrček <jhrcek@redhat.com>
2018-12-15 13:56:28 +0100

    Decompose BuildDurations

--- scraper/src/BuildDurations.hs
+++ scraper/src/BuildDurations.hs
@@ -1,4 +1,4 @@
-module BuildDurations where
+module BuildDurations (printBuildTimesOfFinishedDownstreamPrJobs) where
 
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
@@ -7,14 +7,14 @@ import qualified Jenkins as J
 import qualified Statistics.Sample as Stat
 import qualified Util
 
-import Conduit (filterC, iterMC, mapM_C, runConduit, yieldMany, (.|))
+import Conduit (filterC, mapM_C, runConduit, yieldMany, (.|))
 import Data.List (genericLength)
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import Data.Time.Clock (secondsToDiffTime)
 import Data.Time.LocalTime (timeToTimeOfDay)
 import Jenkins (BuildResult (SUCCESS, UNSTABLE), BuildStats,
-                FolderUrl (FolderUrl))
+                FolderUrl (FolderUrl), JobUrl)
 
 {-| Scrape durations of downstream PR job builds with result SUCCESS/UNSTABLE builds -}
 printBuildTimesOfFinishedDownstreamPrJobs :: IO ()
@@ -23,23 +23,25 @@ printBuildTimesOfFinishedDownstreamPrJobs = do
     runConduit
         $ yieldMany jobUrls
         .| filterC (Text.isSuffixOf "downstream-pullrequests" . J.getJobName)
-        .| iterMC (\jobUrl -> Text.putStr $  J.getJobName jobUrl <> " | ")
-        .| mapM_C (\jobUrl -> do
-              builds <- J.getAllBuilds jobUrl
-              stats <- traverse J.getBuildStats builds
-              let finishedBuildDurations = J.buildDurationMilis <$> filter isFinished stats
-                  averageDurationMilis = sum finishedBuildDurations `div` genericLength finishedBuildDurations
-              if not (null finishedBuildDurations)
-                  then Text.putStrLn $ Text.intercalate ","
-                          [ "avg = " <> formatDuration averageDurationMilis
-                          , "σ = " <> (formatDuration . round . Stat.stdDev . Vect.fromList $ fmap fromIntegral finishedBuildDurations)
-                          , "n = " <> Util.lengthText finishedBuildDurations
-                          ]
-                  else Text.putStrLn "N/A"
-            )
-      where
-        isFinished :: BuildStats -> Bool
-        isFinished stat = J.buildResult stat `elem` [SUCCESS, UNSTABLE]
+        .| mapM_C printJobStats
 
-        formatDuration :: Integer -> Text
-        formatDuration milis = Text.pack . show . timeToTimeOfDay . secondsToDiffTime $ div milis 1000
+printJobStats :: JobUrl -> IO ()
+printJobStats jobUrl = do
+    Text.putStrLn $ J.getJobName jobUrl
+    builds <- J.getAllBuilds jobUrl
+    stats <- traverse J.getBuildStats builds
+    let finishedBuildDurations = J.buildDurationMilis <$> filter isFinished stats
+        averageDurationMilis = sum finishedBuildDurations `div` genericLength finishedBuildDurations
+    Text.putStrLn $ if null finishedBuildDurations
+        then "    - No finished (green or yellow) builds"
+        else Text.intercalate ", "
+            [ "    - avg = " <> formatDuration averageDurationMilis
+            , "σ = " <> (formatDuration . round . Stat.stdDev . Vect.fromList $ fmap fromIntegral finishedBuildDurations)
+            , "based on " <> Util.lengthText finishedBuildDurations <> " finished build(s)"
+            ]
+  where
+    formatDuration :: Integer -> Text
+    formatDuration milis = Text.pack . show . timeToTimeOfDay . secondsToDiffTime $ milis `div` 1000
+
+    isFinished :: BuildStats -> Bool
+    isFinished stat = J.buildResult stat `elem` [SUCCESS, UNSTABLE]
